e72564b9e1d59618b302c79e3b7143bd535729f9
Gustavo Gutierrez <gusguti04@gmail.com>
2020-04-28 16:53:08 -0700

    Finish queue problems

---
+++ W23/D2/queue_project/lib/queue.js
@@ -0,0 +1,66 @@
+// ============================================================================
+// Implementation Exercise: Queue
+// ============================================================================
+//
+// -------
+// Prompt:
+// -------
+//
+// Implement a Queue and all of its methods below!
+//
+// ------------
+// Constraints:
+// ------------
+//
+// Make sure the time and space complexity of each is equivalent to those
+// in the table provided in the Time and Space Complexity Analysis section
+// of your Stack reading!
+//
+// -----------
+// Let's Code!
+// -----------
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.front = null;
+    this.back = null;
+    this.length = 0;
+  }
+
+  enqueue(val) {
+    const newNode = new Node(val);
+    if (!this.front) {
+      this.front = newNode;
+      this.back = newNode;
+    } else {
+      this.back.next = newNode;
+      this.back = newNode;
+    }
+    return ++this.length;
+  }
+
+  dequeue() {
+    if (!this.front) return null;
+    if (this.front === this.back) {
+      this.back = null;
+    }
+    const tempNode = this.front;
+    this.front = this.front.next;
+    this.length--;
+    return tempNode.value;
+  }
+
+  size() {
+    return this.length;
+  }
+}
+
+exports.Node = Node;
+exports.Queue = Queue;
