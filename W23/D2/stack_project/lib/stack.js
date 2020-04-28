3a145b6c2c8e343ed1e5ae0e4367d059055a3e74
Gustavo Gutierrez <gusguti04@gmail.com>
2020-04-28 16:54:40 -0700

    Finish stack problems

---
+++ W23/D2/stack_project/lib/stack.js
@@ -0,0 +1,72 @@
+// ============================================================================
+// Implementation Exercise: Stack
+// ============================================================================
+//
+// -------
+// Prompt:
+// -------
+//
+// Implement a Stack and all of its methods below!
+//
+// ------------
+// Constraints:
+// ------------
+//
+// Make sure the time and space complexity of each is equivalent to those
+// in the table provided in the Time and Space Complexity Analysis section
+// of your Stack reading!
+//
+// -----------
+// Let's Code!
+// -----------
+
+class Node {
+  constructor(val) {
+    this.value = val;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    let newNode = new Node(val);
+    let tempNode;
+    if (!this.top) {
+      this.top = newNode;
+      this.bottom = newNode;
+    } else {
+      tempNode = this.top;
+      this.top = newNode;
+      this.top.next = tempNode;
+    }
+    this.length++;
+    return this.length;
+    // return ++this.length;
+  }
+
+  pop() {
+    if (!this.top) {
+      return null;
+    }
+    if (this.top === this.bottom) {
+      this.bottom = null;
+    }
+    const temp = this.top;
+    this.top = this.top.next;
+    this.length--;
+    return temp.value;
+  }
+
+  size() {
+    return this.length;
+  }
+}
+
+exports.Node = Node;
+exports.Stack = Stack;
