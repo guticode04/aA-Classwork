5c8bcbe4850d09cfacd72eeec813ca437fff0641
Gustavo Gutierrez <gusguti04@gmail.com>
2020-05-04 17:20:58 -0700

    Finish problems

--- W24/heaps_project/lib/is_heap.js
+++ W24/heaps_project/lib/is_heap.js
@@ -2,8 +2,20 @@
 // [null, 50, 42, 27, 32, 24]
 
 function isMaxHeap(array, idx=1) {
-	// let max = Math.max(...array);
-	// let maxHeap = 
+	let isCompleteHeap = array.every(ele => ele !== undefined);
+	return isCompleteHeap && _isMaxHeap(array);
+}
+
+//helper function
+function _isMaxHeap(arr, idx = 1) {
+	if(arr[idx] === undefined) return true;
+	let leftChildIdx = 2 * idx;
+	let rightChildIdx = 2 * idx + 1;
+	let leftChildVal = arr[leftChildIdx] === undefined ? -Infinity : arr[leftChildIdx];
+	let rightChildVal = arr[rightChildIdx] === undefined ? -Infinity : arr[rightChildIdx];
+
+	return (arr[idx] > leftChildVal && arr[idx] > rightChildVal)
+		&& (_isMaxHeap(arr, leftChildIdx) && _isMaxHeap(arr, rightChildIdx));
 }
 
 
