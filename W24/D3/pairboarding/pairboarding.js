5da0ce1cf3d14ee76472ac5a689952c1ec4b639a
Gustavo Gutierrez <gusguti04@gmail.com>
2020-05-06 21:55:55 -0700

    Complete caesar cipher

--- W24/D3/pairboarding/pairboarding.js
+++ W24/D3/pairboarding/pairboarding.js
@@ -98,3 +98,22 @@ function validIp(string) {
 
   return valid
 }
+
+// caesarCipher
+
+function caesarCipher(str, shift) {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  let encoded = "";
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === ' ') {
+      encoded += ' ';
+      continue;
+    }
+
+    const offset = (alphabet.indexOf(str[i]) + shift) % 26;
+    encoded += alphabet[offset];
+  }
+
+  return encoded;
+}
